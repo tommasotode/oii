b335fa0da5d3fd1cf3238dd96d6b0d76c4072891
TommasoTodescato <tommasotodescato@gmail.com>
2023-06-05 08:37:20 +0200

    BFS implementation

--- algobadge/grafi/interruttori/prova.py
+++ algobadge/grafi/interruttori/prova.py
@@ -1,45 +1,47 @@
-# Adjascency List representation in Python
-
-class AdjNode:
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
-
+from collections import defaultdict
 
 class Graph:
-    def __init__(self, num):
-        self.V = num
-        self.graph = [None] * self.V
+	def __init__(self):
+		self.graph = defaultdict(list)
 
-    # Add edges
-    def add_edge(self, s, d):
-        node = AdjNode(d)
-        node.next = self.graph[s]
-        self.graph[s] = node
+	def addEdge(self, master, vertex):
+		self.graph[master].append(vertex)
 
-        node = AdjNode(s)
-        node.next = self.graph[d]
-        self.graph[d] = node
+	def printGraph(self):
+		for pos in self.graph.keys():
+			print(str(pos) + " -> ", end="")
+			for i in self.graph[pos]:
+				print(str(i) + " -> " , end="")
 
-    # Print the graph
-    def print_agraph(self):
-        for i in range(self.V):
-            print("Vertex " + str(i) + ":", end="")
-            temp = self.graph[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
-            print(" \n")
+			print("\n")
+
+	def BFS(self, current):
+		visited = [False] * (max(self.graph) + 1)
+		visited[current] = True
+		to_visit = []
+		to_visit.append(current)
+
+		# while the queue is not empty
+		while to_visit:
+
+			# pop should be for stacks, but it is sufficient to pop the first element to
+			# make it a queue
+			current = to_visit.pop(0)
+			print(current, end=" ")
+
+			# iterate over the linked list of connections of the current vertex
+			for i in self.graph[current]:
+				if visited[i] == False:
+					to_visit.append(i)
+					visited[i] = True
 
 
-if __name__ == "__main__":
-    V = 5
-
-    # Create graph and edges
-    graph = Graph(V)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 2)
-    graph.add_edge(0, 3)
-    graph.add_edge(1, 2)
-
-    graph.print_agraph()
+if __name__ == '__main__':
+	g = Graph()
+	g.addEdge(0, 1)
+	g.addEdge(0, 2)
+	g.addEdge(1, 2)
+	g.addEdge(2, 0)
+	g.addEdge(2, 3)
+	g.addEdge(3, 3)
+	g.printGraph()
