75fb579526b8b483e169bf11e7ccd6f02421ebf4
Tommaso <tommasotodescato@gmail.com>
2023-06-30 00:19:10 +0200

    completely working

--- algobadge/ds/muraglia/st.cpp
+++ algobadge/ds/muraglia/st.cpp
@@ -20,49 +20,46 @@ class SegmentTree
 
 		void build(vector<int>& data, Node node)
 		{
-			if (node.left == node.right)
-				tree[node.index] = data[node.left]; // caso base
+			if (node.left == node.right)	// caso base
+				tree[node.index] = data[node.left];
 			else
 			{
 				int mid = (node.left + node.right) / 2;
-
 				Node left = {node.index*2, node.left, mid};
 				Node right = {node.index*2 + 1, mid + 1, node.right};
+				
 				build(data, left);
 				build(data, right);
-				
 				tree[node.index] = func(tree[node.index*2], tree[node.index*2 + 1]);
 			}
 		}
 
 		int query(Node node, int l, int r)
 		{
-			if (l > r) 
-				return INT_MIN;
+			if (l > r)	// caso base (minimo in questo caso)
+				return INT_MIN;	// da cambiare in base alla query
 			
-			if (node.left == l && node.right == r)
-				return tree[node.index];
+			if (node.left == l && node.right == r)	// caso base definitivo
+				return tree[node.index];	// quando rientra completamente nel range
 
 			int mid = (node.left + node.right) / 2;
-
 			Node left = {node.index*2, node.left, mid};
 			Node right = {node.index*2 + 1, mid + 1, node.right};
 
-			int tmp_l = query(left, l, min(r, mid));
-			int tmp_r = query(right, max(l, mid + 1), r);
+			int end = min(r, mid), start = max(l, mid + 1);
+			int tmp_l = query(left, l, end);
+			int tmp_r = query(right, start, r);
 
 			return func(tmp_l, tmp_r);
 		}
 
 		void update(Node node, int pos, int val)
 		{
-			if (node.left == node.right)
-				tree[node.index] = val; // caso base
-				
+			if (node.left == node.right)	// caso base
+				tree[node.index] = val;
 			else 
 			{
 				int mid = (node.left + node.right) / 2;
-				
 				Node left = {node.index*2, node.left, mid};
 				Node right = {node.index*2 + 1, mid + 1, node.right};
 
@@ -88,22 +85,26 @@ class SegmentTree
 				while(node.left != node.right)
 				{
 					int mid = node.left + (( node.right - node.left ) / 2);
-					
-					node.index = (tree[node.index*2] > val) ? node.index*2 : node.index*2 + 1;
-					node.right = (tree[node.index*2] > val) ? mid : node.right;
-					node.left = (tree[node.index*2] > val) ? node.left : mid + 1;
+					if(tree[node.index*2] > val)
+					{
+						node.index = node.index*2;
+						node.right = mid;
+					}
+					else
+					{
+						node.index = 2*node.index + 1;
+						node.left = mid + 1;
+					}
 				}
 				return node.left;
 			}
-
 			int mid = node.left + (node.right - node.left) / 2;
-			
 			Node left = {node.index*2, node.left, mid};
 			Node right = {node.index*2, mid + 1, node.right};
+			
 			int result = first_greater(left, l, r, val);
 			if(result != -1)
 				return result;
-
 			return first_greater(right, l , r, val);
 		}
 
@@ -139,13 +140,11 @@ int main()
 	vector<int> tmp = {6, 2, 94, 4, 17};
 
 	int l = 0, r = length - 1;
-
 	SegmentTree s = SegmentTree(tmp, [](int x, int y) -> int { return max(x, y); });
 
-	int m = s.query(l, r);
-
-	int ciaociao = s.first_greater(l, r, 2);
-	cout << ciaociao << endl;
+	cout << s.first_greater(l, r, 94) << endl;
+	s.update(3, 8953);
+	cout << s.first_greater(l, r, 94) << endl;
 
 	return 0;
 }
