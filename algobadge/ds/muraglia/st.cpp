bc0e9da1ceecefc56c77840d384fe52d31034a42
Tommaso <tommasotodescato@gmail.com>
2023-06-29 23:27:04 +0200

    from now on everything could be broken

--- algobadge/ds/muraglia/st.cpp
+++ algobadge/ds/muraglia/st.cpp
@@ -3,97 +3,104 @@
 
 using namespace std;
 
+typedef struct Node
+{
+	int index;
+	int left;
+	int right;
+} Node;
+
 class SegmentTree
 {
 	private:
 		int n;
-		vector<int> t;
+		vector<int> tree;
 		function<int(int, int)> func;
 
-		void build(vector<int>& data, int node, int tl, int tr)
+		void build(vector<int>& data, int node, int l_tr, int r_tr)
 		{
-			if (tl == tr)
-				t[node] = data[tl];
+			if (l_tr == r_tr)
+				tree[node] = data[l_tr]; // caso base
 			else
 			{
-				int tm = (tl + tr) / 2;
-				build(data, node*2, tl, tm);
-				build(data, node*2+1, tm+1, tr);
-				t[node] = func(t[node*2], t[node*2+1]);
+				int mid_seg = (l_tr + r_tr) / 2;
+				build(data, node*2, l_tr, mid_seg);
+				build(data, node*2 + 1, mid_seg + 1, r_tr);
+				
+				tree[node] = func(tree[node*2], tree[node*2 + 1]);
 			}
 		}
 
-		int query(int node, int tl, int tr, int l, int r)
+		int query(int node, int l_tr, int r_tr, int l, int r)
 		{
 			if (l > r) 
 				return INT_MIN;
 			
-			if (l == tl && r == tr)
-				return t[node];
+			if (l_tr == l && r_tr == r)
+				return tree[node];
 
-			int tm = (tl + tr) / 2;
+			int mid_tr = (l_tr + r_tr) / 2;
 
-			return func(query(node*2, tl, tm, l, min(r, tm)), query(node*2+1, tm+1, tr, max(l, tm+1), r));
+			int left = query(node*2, l_tr, mid_tr, l, min(r, mid_tr));
+			int right = query(node*2 + 1, mid_tr + 1, r_tr, max(l, mid_tr + 1), r);
+
+			return func(left, right);
 		}
 
-		void update(int node, int tl, int tr, int pos, int new_val)
+		void update(int node, int l_tr, int r_tr, int pos, int val)
 		{
-			if (tl == tr)
-				t[node] = new_val;
+			if (l_tr == r_tr)
+				tree[node] = val; // caso base
 				
 			else 
 			{
-				int tm = (tl + tr) / 2;
+				int mid_tr = (l_tr + r_tr) / 2;
 				
-				if (pos <= tm)
-					update(node*2, tl, tm, pos, new_val);
+				if (pos <= mid_tr)
+					update(node*2, l_tr, mid_tr, pos, val);
 				else
-					update(node*2+1, tm+1, tr, pos, new_val);
+					update(node*2 + 1, mid_tr + 1, r_tr, pos, val);
 				
-				t[node] = func(t[node*2], t[node*2+1]);
+				tree[node] = func(tree[node*2], tree[node*2 + 1]);
 			}
 		}
 
-		int get_first(int node, int lv, int rv, int l, int r, int x)
+		int first_greater(int node, int l_node, int r_node, int l, int r, int val)
 		{
-			if(lv > r || rv < l)
+			if(l_node > r || r_node < l)
 				return -1;
 			
-			if(l <= lv && rv <= r)
+			if(l_node >= l && r_node <= r)
 			{
-				if(t[node] <= x)
+				if(tree[node] <= val)
 					return -1;
 				
-				while(lv != rv)
+				while(l_node != r_node)
 				{
-					int mid = lv + (rv-lv)/2;
-					if(t[2*node] > x)
-					{
-						node = 2*node;
-						rv = mid;
-					}
-					else
-					{
-						node = 2*node+1;
-						lv = mid+1;
-					}
+					int mid = l_node + (( r_node - l_node ) / 2);
+					
+					node = (tree[node*2] > val) ? node*2 : node*2 + 1;
+					r_node = (tree[node*2] > val) ? mid : r_node;
+					l_node = (tree[node*2] > val) ? l_node : mid + 1;
 				}
-				return lv;
+
+				return l_node;
 			}
 
-			int mid = lv + (rv-lv)/2;
-			int rs = get_first(2*node, lv, mid, l, r, x);
-			if(rs != -1)
-				return rs;
+			int mid = l_node + (r_node-l_node) / 2;
+			
+			int result = first_greater(node*2, l_node, mid, l, r, val);
+			if(result != -1)
+				return result;
 
-			return get_first(2*node+1, mid+1, rv, l ,r, x);
+			return first_greater(node*2 + 1, mid + 1, r_node, l ,r, val);
 		}
 
 	public:
 		SegmentTree(vector<int>& data, function<int(int, int)> query)
 		{
 			n = data.size();
-			t.resize(4*n);
+			tree.resize(n*4);
 			func = query;
 			build(data, 1, 0, n-1);
 		}
@@ -103,9 +110,9 @@ class SegmentTree
 			return query(1, 0, n-1, l, r);
 		}
 
-		void update(int pos, int new_val)
+		void update(int pos, int val)
 		{
-			update(1, 0, n-1, pos, new_val);
+			update(1, 0, n-1, pos, val);
 		}
 };
 
