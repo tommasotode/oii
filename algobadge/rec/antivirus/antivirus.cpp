43d7a26e5d2c885fd086838f8208623372464dbc
Tommaso <tommasotodescato@gmail.com>
2023-06-30 18:19:01 +0200

    WIP tidy up

--- algobadge/rec/antivirus/antivirus.cpp
+++ algobadge/rec/antivirus/antivirus.cpp
@@ -2,51 +2,47 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
-
+#include <bits/stdc++.h>
 using namespace std;
+
 string getPart(int i, string F, int length, int part)
 {
 	string result = "";
+	
 	if((i+part) > length)
-	{
 		return "000";
-	}
+	
 	for(int j=i; j<i+part; j++)
-	{
 		result = result + F[j];
-	}
+	
 	return result;
 }
 
 int findString(int i,  string F, int length, string toFind)
 {
 	if(getPart(i, F, length, toFind.length()) == "000")
-	{
 		return -1;
-	}
 	
 	if(getPart(i, F, length, toFind.length()) == toFind)
-	{
 		return i;
-	}
 	
 	return findString(i+1, F, length, toFind);
 }
 
 void solve(int t)
 {
-    int N1, N2, N3, N4;
-    cin >> N1 >> N2 >> N3 >> N4;
+	int N1, N2, N3, N4;
+	cin >> N1 >> N2 >> N3 >> N4;
 
-    int M;
-    cin >> M;
+	int M;
+	cin >> M;
 
-    string F1, F2, F3, F4;
-    cin >> F1 >> F2 >> F3 >> F4;
+	string F1, F2, F3, F4;
+	cin >> F1 >> F2 >> F3 >> F4;
 
-    int p1, p2, p3, p4;
+	int p1, p2, p3, p4;
 	
-	// APPROCCIO DA MIGLIORARE PIU AVANTI
+	// da migliorare più avanti eventualmente
 	for(int i=0; i<N1; i++)
 	{
 		string part = getPart(i, F1, N1, M);
@@ -60,19 +56,19 @@ void solve(int t)
 		}
 	}
 	
-    cout << "Case #" << t << ": " << p1 << " " << p2 << " " << p3 << " " << p4 << endl;
+	cout << "Case #" << t << ": " << p1 << " " << p2 << " " << p3 << " " << p4 << endl;
 }
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 
-    int T;
-    cin >> T;
+	int T;
+	cin >> T;
 
-    for (int t = 1; t <= T; t++)
+	for (int t = 1; t <= T; t++)
 	{
-        solve(t);
-    }
+		solve(t);
+	}
 }
--- algobadge/rec/cabala/cabala.cpp
+++ algobadge/rec/cabala/cabala.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 #include <bits/stdc++.h>
-
 using namespace std;
 
 bool isValid(long long num)
@@ -21,54 +20,52 @@ bool isValid(long long num)
 	return true;	
 }
 
-void prova(int N, long long num, int M, vector<long long>& solutions)
+void combine(int N, long long num, int M, vector<long long>& solutions)
 {
-    if (isValid(num))
-        solutions.push_back(num%M);
+	bool valid = isValid(num);
+	
+	if (valid)
+		solutions.push_back(num % M);
 
-    if (N <= 0)
-        return;
+	if (N <= 0)
+		return;
 
-	if(isValid(num))
+	if (valid)
 	{
-		prova(N - 1, num * 10 + 3, M, solutions);
-    	prova(N - 1, num * 10 + 6, M, solutions);
-    	prova(N - 1, num * 10 + 9, M, solutions);
-	}else
-	return;
+		combine(N - 1, num * 10 + 3, M, solutions);
+		combine(N - 1, num * 10 + 6, M, solutions);
+		combine(N - 1, num * 10 + 9, M, solutions);
+	}
+	else return;
 
 }
 
 long long occulta(int N, int M)
 {
-	// il massimo possibile che posso avere è 9696969696969...N
-	// il minimo è 363636363636...N	
-	
 	// per aggiungere a destra faccio n*10+x
 
 	vector<long long> solutions;
-	prova(N, 0, M, solutions);
+	combine(N, 0, M, solutions);
 
-    return *max_element(solutions.begin(), solutions.end());
+	return *max_element(solutions.begin(), solutions.end());
 }
-
 
 int main()
 {
-    FILE *fr, *fw;
-    int T, N, M, i;
+	FILE *fr, *fw;
+	int T, N, M, i;
 
-    fr = fopen("input.txt", "r");
-    fw = fopen("output.txt", "w");
-    assert(1 == fscanf(fr, "%d", &T));
-    for (i=0; i<T; i++)
+	fr = fopen("input.txt", "r");
+	fw = fopen("output.txt", "w");
+	assert(1 == fscanf(fr, "%d", &T));
+	for (i=0; i<T; i++)
 	{
-        assert(2 == fscanf(fr, "%d %d", &N, &M));
-        fprintf(fw, "%d ", occulta(N, M));
-    }
+		assert(2 == fscanf(fr, "%d %d", &N, &M));
+		fprintf(fw, "%d ", occulta(N, M));
+	}
 
-    fprintf(fw, "\n");
-    fclose(fr);
-    fclose(fw);
-    return 0;
+	fprintf(fw, "\n");
+	fclose(fr);
+	fclose(fw);
+	return 0;
 }
