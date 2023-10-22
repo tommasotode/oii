c3e9f71210f13576edd9d5ebee447db7bdd99549
Tommaso <tommasotodescato@gmail.com>
2023-10-22 23:13:05 +0200

    progress

--- misc/subsetfight.cpp
+++ misc/subsetfight.cpp
@@ -7,6 +7,12 @@ int K;
 int V[MAXK];
 int dp[MAXK][MAXK];
 
+/*
+	per ogni n da 0 a V[i]:
+		prova includendo
+		prova senza includere
+*/
+
 int subsets(int V[MAXK], int i, int mod, int n, int K)
 {
 	if (i == n)
@@ -15,10 +21,19 @@ int subsets(int V[MAXK], int i, int mod, int n, int K)
 	if (dp[i][mod] != -1)
 		return dp[i][mod];
  
-	int notIncluded = subsets(V, i + 1, mod, n, K);
-	int included = subsets(V, i + 1, (mod + V[i]) % K, n, K);
+	// ora come ora faccio included e notIncluded per tutti
+	// potrebbe avere senso farlo solo per included, però partendo da 0?
+	// ripasso comunque lo stesso modulo, quindi non cambia niente 
+	// ( mod + (V[i]*x) ) % K	-> 	con x = 0	-> come fare mod % K
 
-	return dp[i][mod] = notIncluded + included;
+	for (int x = 1; x <= V[i]; x++)
+	{
+		int notIncluded = subsets(V, i + 1, mod, n, K);
+		int included = subsets(V, i + 1, (mod + V[i]) % K, n, K);
+
+		return dp[i][mod] = notIncluded + included;
+	}
+
 }
 
 int find(int V[MAXK], int n, int K)
@@ -36,6 +51,9 @@ int main()
 //  freopen("output.txt", "w", stdout);
 	assert(1 == scanf("%d", &K));
 	for(int i=0; i<K; i++) assert(1 == scanf("%d", &V[i]));
+
+
+
 
 
 	int V[6] = {1, 2, 2, 3, 3, 3};
