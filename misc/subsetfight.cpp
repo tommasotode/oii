36a6a5b79dbe577fdac72915b36e9e8a0c30d806
Tommaso <tommasotodescato@gmail.com>
2023-10-23 22:28:06 +0200

    doesn't want to work with large numbers

--- misc/subsetfight.cpp
+++ misc/subsetfight.cpp
@@ -1,48 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAXK 100
+#define MAXK 100 + 1
+//#define MAXV 200 + 1
+
+#define MAX_LEN 3000
 
 int K;
 int V[MAXK];
-int dp[MAXK][MAXK];
 
-/*
-	per ogni n da 0 a V[i]:
-		prova includendo
-		prova senza includere
-*/
-
-int subsets(int V[MAXK], int i, int mod, int n, int K)
+int count(int arr[MAX_LEN] , int n , int K)
 {
-	if (i == n)
-		return (mod == 0);
-
-	if (dp[i][mod] != -1)
-		return dp[i][mod];
+	int dp[n + 1][K + 1];
  
-	// ora come ora faccio included e notIncluded per tutti
-	// potrebbe avere senso farlo solo per included, però partendo da 0?
-	// ripasso comunque lo stesso modulo, quindi non cambia niente 
-	// ( mod + (V[i]*x) ) % K	-> 	con x = 0	-> come fare mod % K
-
-	for (int x = 1; x <= V[i]; x++)
+	for (int j = 0; j < K; j++)
 	{
-		int notIncluded = subsets(V, i + 1, mod, n, K);
-		int included = subsets(V, i + 1, (mod + V[i]) % K, n, K);
-
-		return dp[i][mod] = notIncluded + included;
+		dp[n][j] = (j == 0) ? 1 : 0;
 	}
-
+ 
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < K; j++)
+		{
+			dp[i][j] = dp[i + 1][j] + dp[i + 1][(j - arr[i] + K) % K];
+		}
+	}
+ 
+	return dp[0][0];
 }
 
 int find(int V[MAXK], int n, int K)
 {
-	for (int i = 0; i < MAXK; i++)
-		for (int j = 0; j < MAXK; j++)
-			dp[i][j] = -1;
+	int new_arr[MAX_LEN];
+	int ind = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < V[i]; j++)
+		{
+			new_arr[ind] = i + 1;
+			ind++;
+		}
+	}
 
-	return subsets(V, 0, 0, n, K);
+	return count(new_arr, ind, K);
 }
 
 int main()
@@ -52,12 +52,7 @@ int main()
 	assert(1 == scanf("%d", &K));
 	for(int i=0; i<K; i++) assert(1 == scanf("%d", &V[i]));
 
-
-
-
-
-	int V[6] = {1, 2, 2, 3, 3, 3};
-	int res = find(V, 6, 3);
+	int res = find(V, K, K);
 	
 	printf("%d\n", res);
 	return 0;
