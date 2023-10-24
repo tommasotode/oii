a14c473957175593fb43c5255562c491fbd9a5b7
Tommaso <tommasotodescato@gmail.com>
2023-10-24 16:55:23 +0200

    subset fight 30 / 100

--- misc/subsetfight.cpp
+++ misc/subsetfight.cpp
@@ -8,50 +8,28 @@ using namespace std;
 int K;
 int V[MAXK];
 
-//int count(int arr[MAX_LEN] , int n , int K)
-//{
-//	int dp[n + 1][K + 1];
- 
-//	for (int j = 0; j < K; j++)
-//	{
-//		dp[n][j] = (j == 0) ? 1 : 0;
-//	}
- 
-//	for (int i = n - 1; i >= 0; i--)
-//	{
-//		for (int j = 0; j < K; j++)
-//		{
-//			dp[i][j] = dp[i + 1][j] + dp[i + 1][(j - arr[i] + K) % K];
-//		}
-//	}
- 
-//	return dp[0][0];
-//}
-
-
-uint64_t findCnt(int V[MAX_LEN], int n, int K)
+int findCnt(int arr[], int n, int K)
 {
-	uint64_t dp[K];
-	dp[0] = 1;
+    int dp[K] = { 0 };
+    dp[0] = 1;
  
-	for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
 	{
-		uint64_t next_dp[K];
+        int next_dp[K];
  
-		for (int j = 0; j < K; j++)
+        for (int j = 0; j < K; j++)
 		{
-			next_dp[j] = (dp[j] + dp[(j - V[i] + K) % K]) % 1000000000 + 7;
-			cout << next_dp[j] << endl;
-		}
+            next_dp[j] = (dp[j] % (1000000000 + 7)) + (dp[(j - arr[i] + K) % K] % (1000000000 + 7));
+        }
  
-		memcpy(dp, next_dp, sizeof(next_dp));
-	}
+        memcpy(dp, next_dp, sizeof(next_dp));
+    }
  
-	return dp[0];
+    return dp[0];
 }
 
 
-uint64_t find(int V[MAXK], int n, int K)
+int find(int V[MAXK], int n, int K)
 {
 	int new_arr[MAX_LEN];
 	int ind = 0;
@@ -70,11 +48,11 @@ uint64_t find(int V[MAXK], int n, int K)
 int main()
 {
   	//freopen("input.txt", "r", stdin);
-  	freopen("output.txt", "w", stdout);
+  	//freopen("output.txt", "w", stdout);
 	assert(1 == scanf("%d", &K));
 	for(int i=0; i<K; i++) assert(1 == scanf("%d", &V[i]));
 
-	uint64_t res = find(V, K, K);
+	int res = find(V, K, K) % (1000000000 + 7);
 	
 	printf("%d\n", res);
 	return 0;
