337923b5c653cd4a94fbde7560b5f7d95696488f
TommasoTodescato <tommasotodescato@gmail.com>
2023-10-25 13:27:56 +0200

    80/100

--- misc/subsetfight.cpp
+++ misc/subsetfight.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAXK 100 + 1
+#define MAXK (100 + 1)
+#define MAXV 200000
+#define MAX_LEN (MAXK * MAXV)
 
-#define MAX_LEN 2600
+#define MOD (1000000000 + 7)
 
 int K;
 int V[MAXK];
 
 int findCnt(int arr[], int n, int K)
 {
-    int dp[K] = { 0 };
-    dp[0] = 1;
- 
-    for (int i = 0; i < n; i++)
-	{
-        int next_dp[K];
- 
-        for (int j = 0; j < K; j++)
-		{
-            next_dp[j] = (dp[j] % (1000000000 + 7)) + (dp[(j - arr[i] + K) % K] % (1000000000 + 7));
-        }
- 
-        memcpy(dp, next_dp, sizeof(next_dp));
-    }
- 
-    return dp[0];
-}
+	int dp[K] = {0};
+	dp[0] = 1;
 
+	for (int i = 0; i < n; i++)
+	{
+		int next_dp[K];
+
+		for (int j = 0; j < K; j++)
+		{
+			next_dp[j] = (dp[j] % MOD) + (dp[(j - arr[i] + K) % K] % MOD);
+		}
+
+		memcpy(dp, next_dp, sizeof(next_dp));
+	}
+
+	return dp[0];
+}
 
 int find(int V[MAXK], int n, int K)
 {
@@ -47,13 +48,14 @@ int find(int V[MAXK], int n, int K)
 
 int main()
 {
-  	//freopen("input.txt", "r", stdin);
-  	//freopen("output.txt", "w", stdout);
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
 	assert(1 == scanf("%d", &K));
-	for(int i=0; i<K; i++) assert(1 == scanf("%d", &V[i]));
+	for (int i = 0; i < K; i++)
+		assert(1 == scanf("%d", &V[i]));
 
-	int res = find(V, K, K) % (1000000000 + 7);
-	
+	int res = find(V, K, K) % MOD;
+
 	printf("%d\n", res);
 	return 0;
 }
