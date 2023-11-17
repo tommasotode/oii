19789edc92c3a74199724e2f759dcd93db075efd
TommasoTodescato <tommasotodescato@gmail.com>
2023-11-17 10:45:16 +0100

    avg should work

--- ois2023/round1/avg.cpp
+++ ois2023/round1/avg.cpp
@@ -3,36 +3,37 @@ using namespace std;
 
 int main()
 {
-    // ifstream cin("input.txt");
-    // ofstream cout("output.txt");
+	// ifstream cin("input.txt");
+	// ofstream cout("output.txt");
 
-    int N, K;
-    cin >> N >> K;
-    vector<int> P(N);
-    for (int i = 0; i < N; ++i) cin >> P[i];
+	int N, K;
+	cin >> N >> K;
+	vector<uint64_t> P(N);
+	for (int i = 0; i < N; ++i)
+		cin >> P[i];
 
-    uint64_t sum = 0;
-    uint64_t prod = N * K;
-    for (int i = 0; i < N; i++) sum += P[i];
+	uint64_t sum = 0;
+	uint64_t prod = N * K;
+	for (int i = 0; i < N; i++)
+		sum += P[i];
 
-    int ans = 0;
-    if (sum < prod) ans = 1;
-    else if (sum > prod)
-    {
-        sort(P.begin(), P.end());
-        uint64_t to_remove = sum - prod;
+	int r = 1;
+	if (sum < prod) cout << 1 << endl;
+	else if (sum > prod)
+	{
+		sort(P.rbegin(), P.rend());
+		for (int i = 0; i < N; i++)
+		{
+			sum -= (P[i]-1);
+			if (sum <= prod)
+			{
+				cout << r << endl;
+				break;
+			}
+			++r;
+		}
+	}
+	else cout << 0 << endl;
 
-        int i = 0;
-        for (i; i < N; i++)
-        {
-            if (to_remove <= P[i] - 1)
-                break;
-            else
-                to_remove -= P[i] - 1;
-        }
-        ans = i + 1;
-    }
-
-    cout << ans << endl;
-    return 0;
+	return 0;
 }
