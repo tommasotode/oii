87ed9bfdb62fcac409ad7c64369ea1ef97c81a1c
TommasoTodescato <tommasotodescato@gmail.com>
2023-11-17 11:10:01 +0100

    cct WIP

--- misc/cct/cct.cpp
+++ misc/cct/cct.cpp
@@ -1,25 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-int solve(int N, int K, int *V, int *C)
+
+#define MAXN 100000 + 1
+#define MAXK 20 + 1
+#define MAXT 1000 + 1
+
+int *T, *C;
+int N, K;
+
+int cct(int i, int k)
 {
+	if (i > N)
+	{
+		return T[i];
+	}
 	
+	bool canSkip;
+	canSkip = (k >= K);
+
+	int notSkipped = T[i] + cct(i+1, k+C[i]);
+
+	int skipped = INT_MAX;
+	if (canSkip)
+	{
+		skipped = T[i] + cct(i+1, 0);
+	}
+
+	return min(notSkipped, skipped);
+}
+
+int solve(int N, int K, int *T, int *C)
+{
+
 	return 42;
 }
 
 int main()
 {
-	FILE *fr = stdin;
-	FILE *fw = stdout;
-	int N, K;
+	FILE *fr = stdin, *fw = stdout;
 	assert(fscanf(fr, "%d %d", &N, &K));
-	int *V = new int[N];
-	int *C = new int[N];
-	for (int i = 0; i < N; i++)
-		assert(fscanf(fr, "%d", V + i) == 1);
-	for (int i = 0; i < N; i++)
-		assert(fscanf(fr, "%d", C + i) == 1);
-	fprintf(fw, "%d", solve(N, K, V, C));
-	delete[] V;
+	T = new int[N];
+	C = new int[N];
+	
+	for (int i = 0; i < N; i++) assert(fscanf(fr, "%d", T + i) == 1);
+	for (int i = 0; i < N; i++) assert(fscanf(fr, "%d", C + i) == 1);
+	fprintf(fw, "%d", solve(N, K, T, C));
+	delete[] T;
 	delete[] C;
 	return 0;
 }
