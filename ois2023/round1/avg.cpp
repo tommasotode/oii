00364f7a847b94b5f4035a387f09d40660852f41
TommasoTodescato <tommasotodescato@gmail.com>
2023-11-18 10:22:32 +0100

    avg fixed

--- ois2023/round1/avg.cpp
+++ ois2023/round1/avg.cpp
@@ -3,37 +3,35 @@ using namespace std;
 
 int main()
 {
-	// ifstream cin("input.txt");
-	// ofstream cout("output.txt");
+    // ifstream cin("input.txt");
+    // ofstream cout("output.txt");
 
-	int N, K;
-	cin >> N >> K;
-	vector<uint64_t> P(N);
-	for (int i = 0; i < N; ++i)
-		cin >> P[i];
+    uint64_t N, K;
+    cin >> N >> K;
+    vector<uint64_t> P(N);
+    for (int i = 0; i < N; ++i) cin >> P[i];
 
-	uint64_t sum = 0;
-	uint64_t prod = N * K;
-	for (int i = 0; i < N; i++)
-		sum += P[i];
+    uint64_t sum = 0;
+    uint64_t prod = N * K;
+    for (int i = 0; i < N; i++) sum += P[i];
 
-	int r = 1;
-	if (sum < prod) cout << 1 << endl;
-	else if (sum > prod)
-	{
-		sort(P.rbegin(), P.rend());
-		for (int i = 0; i < N; i++)
-		{
-			sum -= (P[i]-1);
-			if (sum <= prod)
-			{
-				cout << r << endl;
-				break;
-			}
-			++r;
-		}
-	}
-	else cout << 0 << endl;
+    uint64_t r = 1;
+    if (sum < prod) cout << r << endl;
+    else if (sum > prod)
+    {
+        sort(P.rbegin(), P.rend());
+        for (int i = 0; i < N; i++)
+        {
+            sum -= (P[i] - 1);
+            if (sum <= prod)
+            {
+                cout << r << endl;
+                break;
+            }
+            r++;
+        }
+    }
+    else cout << 0 << endl;
 
-	return 0;
+    return 0;
 }
