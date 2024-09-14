d7b8ee7d498e0459407280b58652cd86c76ec99c
tommasotode <tommasotodescato@gmail.com>
2024-09-14 19:53:21 +0200

    ok

---
+++ training/ois/quadrilateral/quadrilateral.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ifstream cin("input.txt");
+
+  int N;
+  cin >> N;
+
+  vector<long long> S(N);
+  for (int i = 0; i < N; ++i)
+    cin >> S[i];
+
+  sort(S.begin(), S.end());
+  for (int i = 0; i <= N - 4; i++)
+  {
+    if (S[i] + S[i+1] + S[i+2] > S[i + 3])
+    {
+      cout <<S[i]<<" "<<S[i+1]<<" "<<S[i+2]<<" "<<S[i+3]<<endl;
+      return 0;
+    }
+  }
+
+  cout << -1 << endl;
+  return 0;
+}
