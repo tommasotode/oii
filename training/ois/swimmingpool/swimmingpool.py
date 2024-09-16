96bef346ffc5faf5cf56938cbd8e7a152bec774b
tommasotode <tommasotodescato@gmail.com>
2024-09-16 12:32:54 +0200

    swp

--- training/ois/swimmingpool/swimmingpool.py
+++ training/ois/swimmingpool/swimmingpool.py
@@ -3,26 +3,10 @@ import statistics
 
 N = int(input().strip())
 P = list(map(int, input().strip().split()))
-
 P.sort()
 
-med1 = int(statistics.median_high(P))
-m, nearestval1 = 10000000, 0
+m = 10000000
 for i in P:
-    if abs(med1 - i) <= m:
-        m = abs(med1 - i)
-
-        if max(i-P[0], P[-1]-i) < max(nearestval1-P[0], P[-1]-nearestval1):
-            nearestval1 = i
-
-med2 = int(statistics.median_low(P))
-m, nearestval2 = 10000000, 0
-for i in P:
-    if abs(med1 - i) <= m:
-        m = abs(med1 - i)
-
-        if max(i-P[0], P[-1]-i) < max(nearestval2-P[0], P[-1]-nearestval2):
-            nearestval2 = i
-
-sol = min( max(nearestval1-P[0], P[-1]-nearestval1), max(nearestval2-P[0], P[-1]-nearestval2) )
-print(sol)
+    m = min(m, max(i-P[0], P[-1]-i))
+        
+print(m)
