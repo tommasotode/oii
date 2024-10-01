8ca8ed665c48215b4e67d4013fad8b00ec5ceafb
tommasotode <tommasotodescato@gmail.com>
2024-10-01 11:20:52 +0200

    squareunion 35

--- training/ois/squareunion/squareunion.cpp
+++ training/ois/squareunion/squareunion.cpp
@@ -6,19 +6,31 @@ int main()
     ifstream cin("input.txt");
     // ofstream cout("output.txt");
 
-    int N;
+    uint64_t N;
     cin >> N;
-
-    vector<pair<int,int>> rects(N);
-    for (int i = 0; i < N; ++i)
-        cin >> rects[i].first;
+    vector<pair<uint64_t, uint64_t>> rects(N);
     for (int i = 0; i < N; ++i)
         cin >> rects[i].second;
+    for (int i = 0; i < N; ++i)
+        cin >> rects[i].first;
+    
+    // first : raggio
+    // second : centro
 
-    sort(rects.begin(), rects.end());
+    unordered_map<uint64_t, uint64_t> m;
+    for (auto &r : rects)
+    {
+        for (int j = r.second - r.first; j < r.second + r.first; j++)
+        {
+            if (m[j] < r.first)
+                m[j] = r.first;
+        }
+    }
 
-    cout << 0 << endl;
+    uint64_t sum = 0;
+    for (const auto &[k, v] : m)
+        sum += v;
 
-
+    cout << sum * 2 << endl;
     return 0;
 }
