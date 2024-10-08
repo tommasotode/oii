7d0e7eb38a576e58d9b5f8d1ad6c854ad5b97ac9
tommasotode <tommasotodescato@gmail.com>
2024-10-08 11:35:20 +0200

    lights

--- training/ois/lights/lights.cpp
+++ training/ois/lights/lights.cpp
@@ -1,38 +1,43 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+int shortestSegmentWithAllColors(int N, int C, const vector<int> &L)
+{
+    int res = INT_MAX;
+
+    unordered_map<int, int> colors;
+    int count = 0;
+    int l = 0;
+    for (int r = 0; r < N; ++r)
+    {
+        if (colors[L[r]] == 0)
+            count++;
+        colors[L[r]]++;
+
+        while (count == C)
+        {
+            res = min(res, r - l + 1);
+            colors[L[l]]--;
+            if (colors[L[l]] == 0)
+                count--;
+            l++;
+        }
+    }
+
+    return res;
+}
 
 int main()
 {
-    ifstream cin("input.txt");
+    // ifstream cin("input.txt");
     // ofstream cout("output.txt");
 
     int N, C;
     cin >> N >> C;
     vector<int> L(N);
     for (int i = 0; i < N; i++)
-    {
         cin >> L[i];
-    }
 
-    int count = C;
-    while (count < N)
-    {
-        int i = 0;
-        while (i < N-count)
-        {
-            set<int> s;
-            for (int j = i; j < i+count; j++)
-                s.insert(L[j]);
-
-            if (s.size() == C){
-                cout << count << endl;
-                return 0;
-            }
-            i++;
-        }
-        count++;
-    }
-
-    cout << count << endl;
+    cout << shortestSegmentWithAllColors(N, C, L) << endl;
+    return 0;
 }
