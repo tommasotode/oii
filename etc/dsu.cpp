a976470ba128682033b89b5212f9a8a4e7ee347e
tommasotode <tommasotodescato@gmail.com>
2024-11-11 11:42:12 +0100

    ok

--- etc/dsu.cpp
+++ etc/dsu.cpp
@@ -4,35 +4,35 @@ using namespace std;
 class dsu
 {
 private:
-	int N;
-	vector<int> parent;
+  int N;
+  vector<int> parent;
 
 public:
-	dsu(int size)
-	{
-		N = size;
-		parent = vector<int>(N);
-		for (int i = 0; i < N; i++)
-			parent[i] = i;
-	}
+  dsu(int size)
+  {
+    N = size;
+    parent = vector<int>(N);
+    for (int i = 0; i < N; i++)
+      parent[i] = i;
+  }
 
-	void make_set(int v)
-	{
-		parent[v] = v;
-	}
+  void make_set(int v)
+  {
+    parent[v] = v;
+  }
 
-	int find_set(int v)
-	{
-		if (v == parent[v])
-			return v;
-		return find_set(parent[v]);
-	}
+  int find_set(int v)
+  {
+    if (v == parent[v])
+      return v;
+    return find_set(parent[v]);
+  }
 
-	void union_sets(int a, int b)
-	{
-		a = find_set(a);
-		b = find_set(b);
-		if (a != b)
-			parent[b] = a;
-	}
+  void union_sets(int a, int b)
+  {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+      parent[b] = a;
+  }
 };
